2de8a768e1f5faaf25cdefd35d25604549381ef3
Santos Rivera <srivera145@gmail.com>
2022-11-14 15:04:48 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: [
+      'yt3.ggpht.com',
+      'lh3.googleusercontent.com'    
+    ],
+  }
 }
 
 module.exports = nextConfig
